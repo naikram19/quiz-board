c8ad0101ff0962d9bb9d180db7e15fcea0df905f
Ram Naik <naikram1986@gmail.com>
2018-09-25 00:53:22 +0300

    added javascript and html

---
+++ js/script.js
@@ -0,0 +1,21 @@
+  function myQuiz(){
+    var question1 = $("input:radio[name=question1]:checked").val();
+    var question2 = $("input:radio[name=question2]:checked").val();
+    var question3 = $("input:radio[name=question3]:checked").val();
+    var question4 = $("input:radio[name=question4]:checked").val();
+
+    var marks = 0;
+    if(question1 === "1") {
+      marks++;
+    }
+    if (question2 === "1") {
+      marks++;
+    }
+    if (question3 === "1") {
+      marks++;
+    }
+    if (question4 === "1") {
+      marks++;
+    }
+    alert(marks)
+  };
